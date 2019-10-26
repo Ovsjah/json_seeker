a9d98248ecaca46c106c0a60834bca35f9c93c7d
Ovsjah <ovsjah@gmail.com>
2019-10-27 00:48:14 +0300

    Rename method

--- app/controllers/json_seeker_controller.rb
+++ app/controllers/json_seeker_controller.rb
@@ -1,7 +1,7 @@
 class JsonSeekerController < ApplicationController
   def seek
     all = parse('data.json')  # instead of creating a model with db uses the public json file
-    @found = params[:search] ? all.select { |hsh| seek_in(hsh) } : all  # seeks for result in collection of hashes or takes all if there are no params
+    @found = params[:search] ? all.select { |hsh| present_in?(hsh) } : all  # seeks for result in collection of hashes or takes all if there are no params
   end
 
   private
@@ -10,7 +10,7 @@ class JsonSeekerController < ApplicationController
       JSON.parse(File.read("#{Rails.root}/public/#{file_name}"))
     end
 
-    def seek_in(hsh)
+    def present_in?(hsh)
       params[:search].downcase.split.all? do |seek_for|  # splits params[:search] into array of downcased words and checks for their presence
         search_str = hsh.values.join(' ').downcase  # in the search string
         if seek_for.start_with?('-')  # supports negative search
